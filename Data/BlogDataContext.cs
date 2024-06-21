883cfd05efd60ceb0b97e6a58f79c234d8797a11
Juan Holy <juanholydesouza@gmail.com>
2024-06-21 04:12:58 -0400

    feat: adding data configuration

---
+++ Data/BlogDataContext.cs
@@ -0,0 +1,21 @@
+using Blog.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Data
+{
+    public class BlogDataContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        // public DbSet<PostTag> PostTags { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<User> Users { get; set; }
+        // public DbSet<UserRole> UserRoles { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("server=localhost;database=blog;user=root;password=;",new MySqlServerVersion(new Version(8,0,2)))   ;
+        }
+    }
+}
